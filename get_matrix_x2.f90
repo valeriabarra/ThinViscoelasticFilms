2d56eb40660b7b0917e34a62054e19239ede1582
Valeria Barra <39932030+valeriabarra@users.noreply.github.com>
2019-06-18 08:28:20 -0600

    Update LICENSE

--- get_matrix_x2.f90
+++ get_matrix_x2.f90
@@ -1,7 +1,29 @@
+! BSD 2-Clause (MIT) License
+!
+! Copyright (c) [2019] [ThinViscoelasticFilms]
+!
+! Permission is hereby granted, free of charge, to any person obtaining a copy
+! of this software and associated documentation files (the "Software"), to deal
+! in the Software without restriction, including without limitation the rights
+! to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+! copies of the Software, and to permit persons to whom the Software is
+! furnished to do so, subject to the following conditions:
+!
+! The above copyright notice and this permission notice shall be included in all
+! copies or substantial portions of the Software.
+!
+! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+! OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+! SOFTWARE.
+
 SUBROUTINE get_matrix_x2(dt,ma_x,ffx,hxxx,hx,ffx0,ffxm,hxxxp&
-,hxxx0,hxxxm,hxxxmm,hx0,hxm,hxavem,hxave0,hxavep,fvdwx,fvdwx0&
-,fvdwxm, fquadvdwx,fquadvdwx0,fquadvdwxm,fquad,fderquadx0,fderquadxm&
-,coeff_one,coeff_three)
+                        ,hxxx0,hxxxm,hxxxmm,hx0,hxm,hxavem,hxave0,hxavep,fvdwx,fvdwx0&
+                        ,fvdwxm, fquadvdwx,fquadvdwx0,fquadvdwxm,fquad,fderquadx0,fderquadxm&
+                        ,coeff_one,coeff_three)
   USE nrtype
   USE domain_time
   USE domain_space, ONLY: n, nmmax
