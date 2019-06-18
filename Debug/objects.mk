9a1a911923c06f220a12090f21b6316d70bc5367
Valeria Barra <39932030+valeriabarra@users.noreply.github.com>
2019-06-18 08:44:07 -0600

    Update objects.mk

--- Debug/objects.mk
+++ Debug/objects.mk
@@ -20,9 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
 
 USER_OBJS :=
 
