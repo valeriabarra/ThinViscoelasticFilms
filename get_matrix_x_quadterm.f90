c9c115a908133b3513deac963226368f23eebadc
Valeria Barra <39932030+valeriabarra@users.noreply.github.com>
2019-06-18 08:29:27 -0600

    Update LICENSE

--- get_matrix_x_quadterm.f90
+++ get_matrix_x_quadterm.f90
@@ -1,5 +1,27 @@
+! BSD 2-Clause (MIT) License
+!
+! Copyright (c) [2019] [ThinViscoelasticFilms]
+!
+! Permission is hereby granted, free of charge, to any person obtaining a copy
+! of this software and associated documentation files (the "Software"), to deal
+! in the Software without restriction, including without limitation the rights
+! to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+! copies of the Software, and to permit persons to whom the Software is
+! furnished to do so, subject to the following conditions:
+!
+! The above copyright notice and this permission notice shall be included in all
+! copies or substantial portions of the Software.
+!
+! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+! OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+! SOFTWARE.
+
 SUBROUTINE get_matrix_x_quadterm(fquad,fderquadx0,fderquadxm,hxxx,hxxxp,hxxx0,hxxxm&
-,hxxxmm,gpp_quadterm,gp_quadterm,g0_quadterm,gm_quadterm,gmm_quadterm)
+                                ,hxxxmm,gpp_quadterm,gp_quadterm,g0_quadterm,gm_quadterm,gmm_quadterm)
   USE nrtype
   USE domain_space, ONLY: n, nmmax
   USE paras, ONLY: coef_slip,dx4
